afe632f6c245f30a39983c84d22197e31513b3b8
Maxim Yunusov <yunus_max@mail.ru>
2011-07-03 16:35:39 +0700

    TaskUn-2: Maintain employee (backend) Add Logger mechanism (slf4j)

--- dev/webapp/src/main/java/org/maxur/taskun/domain/EmployeeFactory.java
+++ dev/webapp/src/main/java/org/maxur/taskun/domain/EmployeeFactory.java
@@ -1,12 +1,19 @@
 package org.maxur.taskun.domain;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 /**
  * @author Maxim Yunusov
  * @version 1.0 7/3/11
  */
 public class EmployeeFactory {
 
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeFactory.class);
+
     public Employee create() {
+        logger.debug("New Employee was created ");
         return new Employee();
     }
 }
